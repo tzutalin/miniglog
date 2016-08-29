e0dc8dfa0fbbcf42303f8e41d9dd6b74a4b723a5
tzutalin <tzu.ta.lin@gmail.com>
2016-08-29 09:50:42 +0800

    Update test.cpp

--- example/test/test.cpp
+++ example/test/test.cpp
@@ -1,7 +1,15 @@
 #include <glog/logging.h>
 
 int main() {
-    google::InitGoogleLogging("Test");
-    LOG(INFO) << "Dump log test";
-    return 0;
+  google::InitGoogleLogging((char *)"Test");
+  LOG(INFO) << "Dump log test";
+
+  CHECK_NE(1, 2) << ": The world must be ending!";
+  // Check if it is euqual
+  CHECK_EQ(std::string("abc")[1], 'b');
+
+  int x = 2;
+  int y = 1;
+  LOG_IF(ERROR, x > y) << "This should be also OK";
+  return 0;
 }
