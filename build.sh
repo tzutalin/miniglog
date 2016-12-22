0e02698c2d9722a7552139622b2e6278bb6f7729
tzutalin <tzu.ta.lin@gmail.com>
2016-12-22 12:31:51 +0800

    Support ninja build

--- build.sh
+++ build.sh
@@ -5,13 +5,23 @@ ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo '===================================================='
 echo '============== Build Destkop version ==============='
 echo '===================================================='
-if [ -x "$(command -v cmake)" ]; then
+
+
+if [ -x "$(command -v ninja)" ]; then
     mkdir build; cd build
-    cmake -D CMAKE_INSTALL_PREFIX=. ..; make -j8; make install
-    cd ../example/test/; cmake .;make
+    cmake -G Ninja -D CMAKE_INSTALL_PREFIX=. ..; ninja;ninja install
+    cd ../example/test/; cmake -G Ninja .;ninja
     ./test
 else
-    echo %ERRORLEVEL%'No CMake'
+    echo %ERRORLEVEL%'No Ninja. Try to use cmake'
+    if [ -x "$(command -v cmake)" ]; then
+        mkdir build; cd build
+        cmake -D CMAKE_INSTALL_PREFIX=. ..; make -j8; make install
+        cd ../example/test/; cmake .;make
+        ./test
+    else
+        echo %ERRORLEVEL%'No CMake'
+    fi
 fi
 
 echo '===================================================='
